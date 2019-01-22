a8555be9b71fd112dc684a1e2109ce34712d9609
farhan <daya@daya-PC.attlocal.net>
2019-01-22 11:48:46 -0500

    subtraction program added

---
+++ githubdemo/src/githubdemo/Subtraction.java
@@ -0,0 +1,14 @@
+package githubdemo;
+
+public class Subtraction {
+	
+	public static int sub(int a, int b) {
+		return a-b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c=sub(5,2);
+		System.out.println("sub of two numbers:"+c);
+	}
+
+}
