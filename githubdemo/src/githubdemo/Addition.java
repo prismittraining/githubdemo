17f14dd68a1ccc02c3dd55e349ac7dadcf21fc49
farhan <daya@daya-PC.attlocal.net>
2019-01-22 10:44:59 -0500

    added sum of 2 no's java file

---
+++ githubdemo/src/githubdemo/Addition.java
@@ -0,0 +1,14 @@
+package githubdemo;
+
+public class Addition {
+	
+	static int add(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c=add(1,2);
+		System.out.println("sum of two numbers is:"+c);
+	}
+
+}
